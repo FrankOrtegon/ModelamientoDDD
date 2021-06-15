62e1fae4075114bddb932bc59c18953f95d4a719
FrankOrtegon <frank.ortegon01@gmail.com>
2021-06-15 16:35:32 -0500

    Se desarrolla los Identity del agregado cliente

--- src/main/java/co/com/sofka/ddd/cliente/values/MembresiaID.java
+++ src/main/java/co/com/sofka/ddd/cliente/values/MembresiaID.java
@@ -3,4 +3,15 @@ package co.com.sofka.ddd.cliente.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class MembresiaID extends Identity {
+
+    public MembresiaID() {
+    }
+
+    private MembresiaID(String id) {
+        super(id);
+    }
+
+    public static MembresiaID of(String id) {
+        return new MembresiaID(id);
+    }
 }
